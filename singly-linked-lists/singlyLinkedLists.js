6105d344a342aad305b6d88c803e30767d6d6bcd
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-12-03 17:08:31 -0300

    finish singly linked list

--- singly-linked-lists/singlyLinkedLists.js
+++ singly-linked-lists/singlyLinkedLists.js
@@ -84,6 +84,102 @@ class SinglyLinkedList {
 
         return this
     }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined
+
+        let current = this.head
+
+        for (let i = 0; i < index; i++) {
+            current = current.next
+        }
+
+        return current
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index)
+
+        if (foundNode) {
+            foundNode.val = val
+            return true
+        }
+
+        return false
+    }
+
+    insert(index, val) {
+        if (index === this.length) {
+            this.push(val)
+            return true
+        }
+
+        if (index === 0) {
+            this.unshift(val)
+            return true
+        }
+
+        let newNode = new Node(val)
+
+        let leftNode = this.get(index - 1)
+
+        if (!leftNode) return false
+
+        let currentIndexNode = leftNode.next
+        leftNode.next = newNode
+        newNode.next = currentIndexNode
+
+        this.length++
+
+        return true
+    }
+
+    remove(index) {
+        if (index === 0) return this.shift()
+        if (index === this.length - 1) return this.pop()
+
+        let leftNode = this.get(index - 1)
+
+        if (!leftNode) return undefined
+
+        let node = leftNode.next
+
+        leftNode.next = node.next
+
+        this.length--
+
+        return node
+    }
+
+    print() {
+        let arr = []
+
+        let current = this.head
+
+        for (let i = 0; i < this.length; i++) {
+            arr.push(current.val)
+            current = current.next
+        }
+
+        return arr
+    }
+
+    reverse() {
+        let node = this.head
+        this.head = this.tail
+        this.tail = node
+
+        let next = null
+        let prev = null
+        for (let i = 0; i < this.length; i++) {
+            next = node.next
+            node.next = prev
+            prev = node
+            node = next
+        }
+
+        return this
+    }
 }
 
 let list = new SinglyLinkedList()
@@ -94,4 +190,4 @@ list.push('teste3')
 
 console.log(list)
 
-console.log(list.unshift('teste0'))
+console.log(list.print())
