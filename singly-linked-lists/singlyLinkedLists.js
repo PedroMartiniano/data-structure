7fff38e0f860f07d974e54e6b613ad1ab98ea3c4
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-12-03 13:21:50 -0300

    singly linked list

--- singly-linked-lists/singlyLinkedLists.js
+++ singly-linked-lists/singlyLinkedLists.js
@@ -40,8 +40,6 @@ class SinglyLinkedList {
             current = current.next
         }
 
-        console.log(current)
-
         if (this.length === 1) {
             this.head = null
             this.tail = null
@@ -54,18 +52,46 @@ class SinglyLinkedList {
 
         return current
     }
+
+    shift() {
+        if (!this.head) return undefined
+
+        let latestHead = this.head
+
+        this.head = latestHead.next
+
+        if (this.length === 1) {
+            this.tail = null
+        }
+
+        this.length--
+
+        return latestHead
+    }
+
+    unshift(val) {
+        let newNode = new Node(val)
+
+        if (this.length === 0) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+
+        this.length++
+
+        return this
+    }
 }
 
 let list = new SinglyLinkedList()
 
-list.push('teste')
-// list.push('teste2')
-// list.push('teste3')
-
-// list.traverse()
-
-console.log(list.pop())
+list.push('teste1')
+list.push('teste2')
+list.push('teste3')
 
 console.log(list)
 
-// console.log(list.tail)
+console.log(list.unshift('teste0'))
