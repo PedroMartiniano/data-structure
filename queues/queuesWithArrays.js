31002681562ad1d1ff5d756dedac4bab9bf18568
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-12-06 19:28:51 -0300

    queues with array

---
+++ queues/queuesWithArrays.js
@@ -0,0 +1,28 @@
+// First think in must be the first think out
+
+// -------------------------------------------
+// using arrays
+
+// Must use push and shift together
+
+let queue = []
+
+queue.push('FIRST')
+queue.push('SECOND')
+queue.push('THIRD')
+
+queue.shift()
+queue.shift()
+queue.shift()
+
+// Must use unshift and pop together
+
+let queue2 = []
+
+queue2.unshift('FIRST')
+queue2.unshift('SECOND')
+queue2.unshift('THIRD')
+
+queue2.pop()
+queue2.pop()
+queue2.pop()
