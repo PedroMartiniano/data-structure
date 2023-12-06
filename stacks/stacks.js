4905c1feaf921673d7c5b9ab701e48c6a2defca6
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-12-05 23:24:09 -0300

    stacks

---
+++ stacks/stacks.js
@@ -0,0 +1,29 @@
+// Last think in must be the first think out
+
+// -------------------------------------------
+
+// Must use push and pop together
+
+let stack = []
+
+stack.push('google')
+stack.push('instagram')
+stack.push('youtube')
+
+stack.pop()
+stack.pop()
+stack.pop()
+
+// -------------------------------------------
+
+// Must use unshift and shifit together (less efficiency)
+
+let stack2 = []
+
+stack2.unshift('new file')
+stack2.unshift('resized file')
+stack2.unshift('cloned out wrinkle')
+
+stack2.shift()
+stack2.shift()
+stack2.shift()
